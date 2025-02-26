e1b7a75670dba18159f57a15832968032c4bc15b
Loi Pham <loi9a2thcsttt@gmail.com>
2025-02-26 13:31:56 +0700

    init blazor app
    init blazor app

---
+++ BlazorAppDeepDive/ToDoApp/Models/ToDoItem.cs
@@ -0,0 +1,26 @@
+﻿namespace ToDoApp.Models
+{
+    public class ToDoItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public bool _isCompleted { get; set; }
+        public bool IsCompleted
+        {
+            get => _isCompleted; 
+            set
+            {
+                _isCompleted = value;
+                if (value)
+                {
+                    DateCompleted = DateTime.Now;
+                }
+                else
+                {
+                    DateCompleted = DateTime.MinValue;
+                }
+            }
+        }
+        public DateTime DateCompleted { get; set; }
+    }
+}
