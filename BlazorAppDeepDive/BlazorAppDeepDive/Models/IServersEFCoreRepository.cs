005fb74c52421f7f2af423a6259c1dcc82ffe5fb
Loi Pham <loi9a2thcsttt@gmail.com>
2025-02-28 10:43:19 +0700

    add database repository
    add database repository

---
+++ BlazorAppDeepDive/BlazorAppDeepDive/Models/IServersEFCoreRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorDeepDive.Models
+{
+    public interface IServersEFCoreRepository
+    {
+        void AddServer(Server server);
+        void DeleteServer(int serverId);
+        Server? GetServerById(int id);
+        List<Server> GetServers();
+        List<Server> GetServersByCity(string cityName);
+        List<Server> SearchServers(string serverFilter);
+        void UpdateServer(int serverId, Server server);
+    }
+}
